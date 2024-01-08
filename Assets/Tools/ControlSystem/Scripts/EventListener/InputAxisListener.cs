420c6b73aaeb7a134f49b773869745c8d9f16e55
yun.wang <wy19910222@hotmail.com>
2024-01-09 03:36:34 +0800

    Input.GetAxis鼠标移动取值范围不是[-1, 1]，此时建议填0.01

--- Assets/Tools/ControlSystem/Scripts/EventListener/InputAxisListener.cs
+++ Assets/Tools/ControlSystem/Scripts/EventListener/InputAxisListener.cs
@@ -13,15 +13,15 @@ namespace Control {
 		public string title;
 		[InputAxesSelect]
 		public string axisName;
-		public float axisValueMin = -1;
-		public float axisValueMax = 1;
-		
+		[Tooltip("鼠标移动取值范围不是[-1, 1]，此时建议填0.01")]
+		public float valueRate = 1;
+
 		[ComponentSelect]
 		public List<ProgressController> progressControllers = new List<ProgressController>() { null };
 		
 		private void Update() {
-			float axisValue = Input.GetAxis(axisName);
-			axisValue = (axisValue - axisValueMin) / (axisValueMax - axisValueMin);
+			float axisValue = Input.GetAxis(axisName) * valueRate;
+			axisValue = axisValue * 0.5F + 0.5F;
 			foreach (var progressController in progressControllers) {
 				if (progressController) {
 					progressController.Progress = axisValue;
