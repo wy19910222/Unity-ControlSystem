21976b21190c1c0e9c18bbdf3acba64a3d1eedf6
yun.wang <wy19910222@hotmail.com>
2024-01-09 15:41:11 +0800

    整理代码

--- Assets/Tools/ControlSystem/Scripts/ProgressControl/BaseProgressCtrlCurve.cs
+++ Assets/Tools/ControlSystem/Scripts/ProgressControl/BaseProgressCtrlCurve.cs
@@ -5,18 +5,38 @@
  * @EditTime: 2023-01-20 04:30:52 038
  */
 
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Control {
 	public abstract class BaseProgressCtrlCurve : BaseProgressCtrl<AnimationCurve> {
-		protected float m_T;
-		
 		protected override AnimationCurve Lerp(AnimationCurve from, AnimationCurve to, float t) {
 			return CurveLerpUtils.CurveLerpUnclamped(from, to, t);
 		}
 
 		protected override float GetT(AnimationCurve from, AnimationCurve to, AnimationCurve value) {
-			return m_T;
+			List<float> tArray = new List<float>();
+			Keyframe[] fromKeys = m_Curve.keys;
+			for (int index = 0, length = fromKeys.Length; index < length; index++) {
+				Keyframe fromKeyframe = fromKeys[index];
+				float fromValue = fromKeyframe.value;
+				float toValue = to.Evaluate(fromKeyframe.time);
+				float valueValue = value.Evaluate(fromKeyframe.time);
+				tArray.Add((valueValue - fromValue) / (toValue - fromValue));
+			}
+			Keyframe[] toKeys = m_Curve.keys;
+			for (int index = 0, length = toKeys.Length; index < length; index++) {
+				Keyframe toKeyframe = toKeys[index];
+				float fromValue = from.Evaluate(toKeyframe.time);
+				float toValue = toKeyframe.value;
+				float valueValue = value.Evaluate(toKeyframe.time);
+				tArray.Add((valueValue - fromValue) / (toValue - fromValue));
+			}
+			float sumT = 0;
+			foreach (float t in tArray) {
+				sumT += t;
+			}
+			return sumT / tArray.Count;
 		}
 
 		protected override bool Equals(AnimationCurve value1, AnimationCurve value2) {
