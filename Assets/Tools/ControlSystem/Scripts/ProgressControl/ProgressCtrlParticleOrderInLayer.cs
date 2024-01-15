c72e7f5b2f5c4675c2f000dc6a61744855f02671
yun.wang <wy19910222@hotmail.com>
2024-01-16 00:44:34 +0800

    整理SortingOrder和OrderInLayer的关系

---
+++ Assets/Tools/ControlSystem/Scripts/ProgressControl/ProgressCtrlParticleOrderInLayer.cs
@@ -0,0 +1,11 @@
+/*
+ * @Author: wangyun
+ * @CreateTime: 2022-09-28 13:58:14 030
+ * @LastEditor: wangyun
+ * @EditTime: 2022-09-28 13:58:14 033
+ */
+
+namespace Control {
+	public class ProgressCtrlParticleOrderInLayer : ProgressCtrlSortingOrder {
+	}
+}
--- Assets/Tools/ControlSystem/Scripts/ProgressControl/ProgressCtrlSortingOrder.cs
+++ Assets/Tools/ControlSystem/Scripts/ProgressControl/ProgressCtrlSortingOrder.cs
@@ -1,17 +1,42 @@
 /*
  * @Author: wangyun
- * @LastEditors: wangyun
- * @Date: 2022-03-30 16:21:41 627
+ * @CreateTime: 2022-09-28 13:58:14 030
+ * @LastEditor: wangyun
+ * @EditTime: 2022-09-28 13:58:14 033
  */
 
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Control {
-	[RequireComponent(typeof(Renderer))]
 	public class ProgressCtrlSortingOrder : BaseProgressCtrlInt {
+		[ComponentSelect]
+		public List<Renderer> renderers = new List<Renderer>();
+
+		protected override void Reset() {
+			base.Reset();
+			renderers.Clear();
+			renderers.AddRange(GetComponents<Renderer>());
+		}
+
+		[ContextMenu("GetRenderersInChildren")]
+		private void GetRenderersInChildren() {
+			renderers.Clear();
+			renderers.AddRange(GetComponentsInChildren<Renderer>(true));
+		}
+		
 		protected override int TargetValue {
-			get => GetComponent<Renderer>().sortingOrder;
-			set => GetComponent<Renderer>().sortingOrder = value;
+			get {
+				foreach (var rdr in renderers) {
+					return rdr.sortingOrder;
+				}
+				return 0;
+			}
+			set {
+				foreach (var rdr in renderers) {
+					rdr.sortingOrder = value;
+				}
+			}
 		}
 	}
 }
